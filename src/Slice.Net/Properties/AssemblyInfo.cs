5c13162b85e3e19c11c72514e450932118af2eda
Selim Waly <selimwalyy@gmail.com>
2024-06-24 17:52:09 +0300

    Define Library Extension properties

---
+++ src/Slice.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+/* AssemblyInfo.cs : Defines the dll properties. 
+ *
+ * Copyright (C) 2024 Selim Waly.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Slice.Net Library")]
+[assembly: AssemblyDescription("Slice.Net is a .Net Library for creating Dual-Screen application on Windows")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Selim Waly")]
+[assembly: AssemblyProduct("Slice.Net")]
+[assembly: AssemblyCopyright("Copyright © Selim Waly 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c2f849cb-c1ef-4103-af05-18e5e4280eb7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
