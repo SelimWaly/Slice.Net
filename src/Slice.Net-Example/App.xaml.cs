dce847cf4ec8b4a71c03e8013b96d75d5a8fa527
Selim Waly <selimwalyy@gmail.com>
2024-06-24 18:11:04 +0300

    Upload program example

---
+++ src/Slice.Net-Example/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿/* App.xaml.cs : Generic App.xaml file
+ *
+ * Copyright (C) 2024 Selim Waly.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfWindowTest
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
